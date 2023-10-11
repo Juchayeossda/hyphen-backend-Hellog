f045511be0ca26e78a9a858847fd996c0f296e6f
JunBumHan <jjerbum@JunBumHans-Laptop.local>
2023-10-11 19:50:13 +0900

    re-initialize

--- exception/panic.go
+++
@@ -1,8 +0,0 @@
-package exception
-
-// error를 검사하여 에러가 있으면 panic발생
-func InspectErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "hyphen-backend-hellog/configuration"
-
-func main() {
-	// setup configuration
-	config := configuration.New()
-	configuration.NewDatabase(config)
-}
