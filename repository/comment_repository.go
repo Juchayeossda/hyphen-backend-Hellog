4c624c98b4bca9c1f5e566691c7aba528e473214
JunBumHan <jjerbum@JunBumHans-Laptop.local>
2023-10-18 18:05:30 +0900

    user repository CR 기능 구현 완료
    Creat(), QueryByID(), QUeryChilds(), QueryParentsByPost() 함수 구현
    나머지를 만들지 않은 까달은 내 엔드포인트에서 더 사용할 것이 없이 때문에

---
+++ repository/comment_repository.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"context"
+	"hyphen-backend-hellog/ent"
+	"hyphen-backend-hellog/ent/comment"
+)
+
+type CommentRepository interface {
+	Create(ctx context.Context, content string, post *ent.Post, author *ent.User, parent *ent.Comment) (*ent.Comment, error)
+	QueryByID(ctx context.Context, id int) (*ent.Comment, error)
+	QueryChilds(ctx context.Context, parent *ent.Comment) ([]*ent.Comment, error)
+	QueryParentsByPost(ctx context.Context, post *ent.Post) ([]*ent.Comment, error)
+}
+
+func NewCommentRepository(database *ent.Client) CommentRepository {
+	return &ICommentRepository{client: database}
+}
+
+type ICommentRepository struct {
+	client *ent.Client
+}
+
+func (r *ICommentRepository) Create(ctx context.Context, content string, post *ent.Post, author *ent.User, parent *ent.Comment) (*ent.Comment, error) {
+	comment := r.client.Comment.Create().
+		SetContent(content).
+		SetPost(post).
+		SetAuthor(author)
+
+	if parent != nil {
+		return comment.
+			SetParent(parent).
+			Save(ctx)
+	}
+
+	return comment.
+		Save(ctx)
+}
+
+func (r *ICommentRepository) QueryByID(ctx context.Context, id int) (*ent.Comment, error) {
+	return r.client.Comment.Get(ctx, id)
+}
+
+func (r *ICommentRepository) QueryChilds(ctx context.Context, parent *ent.Comment) ([]*ent.Comment, error) {
+	return parent.QueryChildrens().All(ctx)
+
+}
+
+func (r *ICommentRepository) QueryParentsByPost(ctx context.Context, post *ent.Post) ([]*ent.Comment, error) {
+	return post.QueryComments().Where(comment.Not(comment.HasParent())).All(ctx)
+}
