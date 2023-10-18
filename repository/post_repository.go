f52658436d6cb36e459f8b0f942c8b4ddee4c286
JunBumHan <jjerbum@JunBumHans-Laptop.local>
2023-10-18 18:04:01 +0900

    feat: post repository CRUD 기능 개발 완료
    Create(), QueryByID(), UpdateByID(), DeleteByID() 함수 구현 완료

---
+++ repository/post_repository.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"context"
+	"hyphen-backend-hellog/ent"
+)
+
+type PostRepository interface {
+	Create(ctx context.Context, title string, content string, previewImage string, isPrivate bool, author *ent.User) (*ent.Post, error)
+	QueryByID(ctx context.Context, id int) (*ent.Post, error)
+	UpdateByID(ctx context.Context, id int, title string, content string, previewImage string, isPrivate bool, author *ent.User) (*ent.Post, error)
+	DelteByID(ctx context.Context, id int) error
+}
+
+func NewPostRepository(database *ent.Client) PostRepository {
+	return &IPostRepository{client: database}
+}
+
+type IPostRepository struct {
+	client *ent.Client
+}
+
+func (r *IPostRepository) Create(ctx context.Context, title string, content string, previewImage string, isPrivate bool, author *ent.User) (*ent.Post, error) {
+	return r.client.Post.Create().
+		SetTitle(title).
+		SetContent(content).
+		SetPreviewImage(previewImage).
+		SetIsPrivate(isPrivate).
+		SetAuthor(author).
+		Save(ctx)
+}
+
+func (r *IPostRepository) QueryByID(ctx context.Context, id int) (*ent.Post, error) {
+	return r.client.Post.Get(ctx, id)
+}
+
+func (r *IPostRepository) UpdateByID(ctx context.Context, id int, title string, content string, previewImage string, isPrivate bool, author *ent.User) (*ent.Post, error) {
+
+	updatePost := r.client.Post.UpdateOneID(1).
+		SetTitle(title).
+		SetContent(content).
+		SetPreviewImage(previewImage).
+		SetIsPrivate(isPrivate)
+
+	if author != nil {
+		return updatePost.
+			SetAuthor(author).
+			Save(ctx)
+	}
+
+	return updatePost.Save(ctx)
+}
+
+func (r *IPostRepository) DelteByID(ctx context.Context, id int) error {
+	return r.client.Post.DeleteOneID(id).
+		Exec(ctx)
+}
