484b1519115e7215d1d5d07840c0d2adc5745e29
JunBumHan <jjerbum@JunBumHans-Laptop.local>
2023-10-18 15:32:14 +0900

    feat: NewDatabase() 구현 완료
    .env에서 각각의 설정 파일을 불러오고 데이터베이스 연결 설정 데이터베이스 idle, open, conn 설정, Auto migration 까지 수행

---
+++ initializer/database.go
@@ -0,0 +1,45 @@
+package initializer
+
+import (
+	"context"
+	"hyphen-backend-hellog/cerrors/exception"
+	"hyphen-backend-hellog/ent"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"entgo.io/ent/dialect/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDatabse(c Component) *ent.Client {
+	username := c.Get("DATASOURCE_USERNAME")
+	password := c.Get("DATASOURCE_PASSWORD")
+	host := c.Get("DATASOURCE_HOST")
+	port := c.Get("DATASOURCE_PORT")
+	dbName := c.Get("DATASOURCE_DB_NAME")
+	maxPoolIdle, err := strconv.Atoi(c.Get("DATASOURCE_POOL_IDLE_CONN"))
+	maxPoolOpen, err := strconv.Atoi(c.Get("DATASOURCE_POOL_MAX_CONN"))
+	maxPollLifeTime, err := strconv.Atoi(c.Get("DATASOURCE_POOL_LIFE_TIME"))
+	exception.Sniff(err)
+
+	dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbName + "?parseTime=true"
+	drv, err := sql.Open("mysql", dsn)
+	exception.Sniff(err)
+
+	// Get the underlying sql.DB object of the driver.
+	db := drv.DB()
+	db.SetMaxIdleConns(maxPoolIdle)
+	db.SetMaxOpenConns(maxPoolOpen)
+	db.SetConnMaxLifetime(time.Duration(rand.Int31n(int32(maxPollLifeTime))) * time.Millisecond)
+	client := ent.NewClient(ent.Driver(drv))
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	return client
+}
